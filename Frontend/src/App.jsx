957ce45ce863e5a78a2bb1bedc2e9e61c3699b25
Bhuvan <bhuvanasairam2005@gmail.com>
2025-09-21 11:43:33 +0530

    Add BrowserRouter to App component for routing setup

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,8 +1,11 @@
 import './App.css'
-
+import { BrowserRouter,Route,Routes } from 'react-router-dom';
 function App() {
   return (
-    <></>
+    <>
+    <BrowserRouter>
+    </BrowserRouter>
+    </>
   )
 }
 export default App;
