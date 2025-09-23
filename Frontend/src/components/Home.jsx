023f657b259c0ae4c26c216082eec7bcbc4964e5
Bhuvan <bhuvanasairam2005@gmail.com>
2025-09-23 22:37:01 +0530

    Add main and footer sections to Home component

--- Frontend/src/components/Home.jsx
+++ Frontend/src/components/Home.jsx
@@ -2,8 +2,14 @@ function Home(){
 return(
     <>
         <header>
-            
+            <h1>University Affiliation management</h1>
         </header>
+        <main>
+
+        </main>
+        <footer>
+            &copy; 2025 University Affiliation Management
+        </footer>
     </>
 )
 }
