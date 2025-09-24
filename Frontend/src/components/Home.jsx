604459ad9c3dafab8a53f025fdae66af892aa3f7
Bhuvan <bhuvanasairam2005@gmail.com>
2025-09-24 14:33:08 +0530

    Refactor Home component to include navigation buttons in the header

--- Frontend/src/components/Home.jsx
+++ Frontend/src/components/Home.jsx
@@ -3,9 +3,15 @@ return(
     <>
         <header>
             <h1>University Affiliation management</h1>
+            <div className="right">
+                    <button className="nav-option">Home</button>
+                    <button className="nav-option">About</button>
+                    <button className="nav-option">Contact Us</button>
+                    <button>Login</button>
+                </div>
         </header>
         <main>
-
+            
         </main>
         <footer>
             &copy; 2025 University Affiliation Management
