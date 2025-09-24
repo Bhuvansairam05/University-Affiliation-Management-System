8869703e185380bc8623b9bb055e8c1c6ca7b49a
Bhuvan <bhuvanasairam2005@gmail.com>
2025-09-24 14:32:11 +0530

    Add back button and structure to Login component

--- Frontend/src/components/Login.jsx
+++ Frontend/src/components/Login.jsx
@@ -1,7 +1,18 @@
 function Login(){
     return(
         <>
-        <header></header>
+        <header>
+            <button>back</button>
+        </header>
+        <main>
+            <div className="login-left"></div>
+            <div className="login-right">
+                <form action="submit"></form>
+            </div>
+        </main>
+        <footer>
+            &copy; 2025 University Affiliation management System
+        </footer>
         </>
     )
 }
