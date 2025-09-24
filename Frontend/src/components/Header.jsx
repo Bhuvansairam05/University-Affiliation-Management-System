1a160ea395b41b09051a62fff46476d9a9722e67
Bhuvan <bhuvanasairam2005@gmail.com>
2025-09-24 14:28:42 +0530

    Add Header component with navigation buttons and title

--- Frontend/src/components/Header.jsx
+++ Frontend/src/components/Header.jsx
@@ -1,6 +1,16 @@
 function Header(){
     return (
-        <></>
+        <>
+            <div className="header">
+                <h1>University Affiliation Management System</h1>
+                <div className="right">
+                    <button className="nav-option">Home</button>
+                    <button className="nav-option">About</button>
+                    <button className="nav-option">Contact Us</button>
+                    <button>Login</button>
+                </div>
+            </div>
+        </>
     );
 }
 export default Header;
