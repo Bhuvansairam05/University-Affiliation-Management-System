a96ffc76010660c8e1d421a8c14d63e59d1007ff
Bhuvan <bhuvanasairam2005@gmail.com>
2025-09-25 22:20:09 +0530

    Add content to About component's main section

--- Frontend/src/components/About.jsx
+++ Frontend/src/components/About.jsx
@@ -10,7 +10,16 @@ function About(){
                     <button>Login</button>
                 </div>
             </header>
-            <main></main>
+            <main>
+                <h1>About Us</h1>
+                <div className="top">
+                    This mainly consists of 
+                    <ul>
+                        <li>License application for new course at collages.</li>
+                        <li>Upgrading license of an existing course at a certain collage.</li>
+                    </ul>
+                </div>
+            </main>
             <footer>
                 &copy; 2025 University Affiliation Management
             </footer>
