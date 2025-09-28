9bc94cc0639abfcc7a1cc52d7bff354ac35cd3c3
Bhuvan <bhuvanasairam2005@gmail.com>
2025-09-28 16:51:57 +0530

    Remove unused BrowserRouter and restructure App component layout with header, main, and footer sections

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,10 +1,22 @@
 import './App.css'
-import { BrowserRouter,Route,Routes } from 'react-router-dom';
 function App() {
   return (
     <>
-    <BrowserRouter>
-    </BrowserRouter>
+    <header>
+            <h1>University Affiliation management</h1>
+            <div className="right">
+                    <button className="nav-option">Home</button>
+                    <button className="nav-option">About</button>
+                    <button className="nav-option">Contact Us</button>
+                    <button>Login</button>
+                </div>
+        </header>
+        <main>
+            
+        </main>
+        <footer>
+            &copy; 2025 University Affiliation Management
+        </footer>
     </>
   )
 }
