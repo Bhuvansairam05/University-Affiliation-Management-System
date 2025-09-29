cbeb087a46be0ea3a097521aea7d1ecea9839922
Bhuvan <bhuvanasairam2005@gmail.com>
2025-09-29 13:18:06 +0530

    Added routes to Home about contact

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,22 +1,22 @@
+import { Link, Outlet } from 'react-router-dom';
+import { Home } from './components/Home';
 import './App.css'
 function App() {
   return (
     <>
-    <header>
-            <h1>University Affiliation management</h1>
-            <div className="right">
-                    <button className="nav-option">Home</button>
-                    <button className="nav-option">About</button>
-                    <button className="nav-option">Contact Us</button>
-                    <button>Login</button>
-                </div>
-        </header>
-        <main>
-            
-        </main>
-        <footer>
-            &copy; 2025 University Affiliation Management
-        </footer>
+      <header>
+        <h1>University Affiliation management</h1>
+        <div className="right">
+          <Link to={Home}><button className="nav-option">Home</button></Link>
+          <Link to={About}><button className="nav-option">About</button></Link>
+          <Link to={Contact}><button className="nav-option">Contact Us</button></Link>
+          <Link to={Login}><button>Login</button></Link>
+        </div>
+      </header>
+      <Outlet />
+      <footer>
+        &copy; 2025 University Affiliation Management
+      </footer>
     </>
   )
 }
--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -4,11 +4,11 @@ import {Routes,BrowserRouter,Route} from 'react-router-dom';
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-
+import Home from './components/Home.jsx';
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <Routes>
-      <Route path='/' element={<App/>}/>
+      <Route path='/' element={<Home/>}/>
       <Route path='/about' element={<About/>}/>
       <Route path='/Login' element={<Login/>}/>
     </Routes>
