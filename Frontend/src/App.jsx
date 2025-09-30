1af5074bc4d6d5e6d8f44d1e384d970abb52bf4c
Bhuvan <bhuvanasairam2005@gmail.com>
2025-09-30 12:21:41 +0530

    Refactor App component to include logo and update header structure

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,11 +1,15 @@
 import { Link, Outlet } from 'react-router-dom';
 import './App.css'
+import Logo from './assets/Logo.jpg';
 function App() {
   return (
     <>
       <header>
-        <h1>University Affiliation management</h1>
-        <div className="right">
+        <div className="header-left">
+          <img src={Logo} alt="Logo" height={80}/>
+          <h1>Gradious Affiliation management</h1>
+        </div>
+        <div className="header-right">
           <Link to="/home"><button className="nav-option">Home</button></Link>
           <Link to="/about"><button className="nav-option">About</button></Link>
           <Link to="/contact"><button className="nav-option">Contact Us</button></Link>
