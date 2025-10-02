ded36e7032eafea427169e9018dbda3199ee5e43
Bhuvan <bhuvanasairam2005@gmail.com>
2025-10-02 16:14:00 +0530

    Refactor Home component to include main structure and headings

--- Frontend/src/components/Home.jsx
+++ Frontend/src/components/Home.jsx
@@ -1,7 +1,11 @@
 function Home(){
 return(
     <>
-        
+        <main>
+            <h1>Welcome to University Affiliation System</h1>
+            <br />
+            <h2>This includes services like...</h2>
+        </main>
     </>
 )
 }
