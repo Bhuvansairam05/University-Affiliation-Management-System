96d5b76b646c88c8b592fa186c696e7a022d7805
Bhuvan <bhuvanasairam2005@gmail.com>
2025-10-03 16:42:44 +0530

    Add HomeContent component and update routing in App component for improved navigation

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,6 +2,7 @@ import Home from './components/Home.jsx';
 import About from './components/About.jsx';
 import Login from './components/Login.jsx';
 import Contact from './components/Contact.jsx';
+import HomeContent from './components/HomeContent.jsx';
 import { Routes, BrowserRouter, Route } from 'react-router-dom';
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
     <BrowserRouter>
     <Routes>
       <Route path='/'  element={<Home/>}>
+        <Route index element={<HomeContent/>}/>
         <Route path='/about' element={<About />} />
         <Route path='/Login' element={<Login />} />
         <Route path='/Contact' element={<Contact/>}/>
---
+++ Frontend/src/components/HomeContent.jsx
@@ -0,0 +1,8 @@
+function HomeContent(){
+    return(
+        <>
+        <h1>welcome to gradious affiliation system.</h1>
+        </>
+    )
+}
+export default HomeContent;
