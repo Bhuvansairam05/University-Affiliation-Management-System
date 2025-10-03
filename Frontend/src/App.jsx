3fe5e4c2b11a3241a1c47a4e5ea3a151cda75cca
Bhuvan <bhuvanasairam2005@gmail.com>
2025-10-03 16:39:43 +0530

    Refactor App and Home components to centralize header and footer structure for improved layout and organization

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,7 +1,3 @@
-import { Link, Outlet } from 'react-router-dom';
-import './App.css'
-import Logo from './assets/Logo.jpg';
-
 import Home from './components/Home.jsx';
 import About from './components/About.jsx';
 import Login from './components/Login.jsx';
@@ -13,30 +9,14 @@ function App() {
 
     <BrowserRouter>
     <Routes>
-      <Route path='/'  element={<App/>}>
-        <Route index element={<Home/>}/>
+      <Route path='/'  element={<Home/>}>
         <Route path='/about' element={<About />} />
         <Route path='/Login' element={<Login />} />
         <Route path='/Contact' element={<Contact/>}/>
       </Route>
     </Routes>
   </BrowserRouter>
-      <header>
-        <div className="header-left">
-          <div className="logo"><img src={Logo} alt="" height={50} width={50}/></div>
-          <h1>Gradious Affiliation management</h1>
-        </div>
-        <div className="header-right">
-          <Link to="/home"><button className="nav-option">Home</button></Link>
-          <Link to="/about"><button className="nav-option">About</button></Link>
-          <Link to="/contact"><button className="nav-option">Contact Us</button></Link>
-          <Link to="/login"><button>Login</button></Link>
-        </div>
-      </header>
-      <Outlet />
-      <footer>
-        &copy; 2025 University Affiliation Management
-      </footer>
+      
     </>
   )
 }
--- Frontend/src/components/Home.jsx
+++ Frontend/src/components/Home.jsx
@@ -1,11 +1,24 @@
+import { Link, Outlet } from 'react-router-dom';
+import Logo from '../assets/Logo.jpg';
 function Home(){
 return(
     <>
-        <main>
-            <h1>Welcome to University Affiliation System</h1>
-            <br />
-            <h2>This includes services like...</h2>
-        </main>
+    <header>
+        <div className="header-left">
+          <div className="logo"><img src={Logo} alt="" height={50} width={50}/></div>
+          <h1>Gradious Affiliation management</h1>
+        </div>
+        <div className="header-right">
+          <Link to="/"><button className="nav-option">Home</button></Link>
+          <Link to="/about"><button className="nav-option">About</button></Link>
+          <Link to="/contact"><button className="nav-option">Contact Us</button></Link>
+          <Link to="/login"><button>Login</button></Link>
+        </div>
+      </header>
+      <Outlet />
+      <footer>
+        &copy; 2025 University Affiliation Management
+      </footer>
     </>
 )
 }
