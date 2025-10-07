8cf6468a21be8cd6e7352d9db478e7bf9da1653b
Bhuvan <bhuvanasairam2005@gmail.com>
2025-10-07 11:38:19 +0530

    Refactor About and Home components; improve header structure and add footer to About component; remove unused Outlet import in Home component

--- Frontend/src/components/About.jsx
+++ Frontend/src/components/About.jsx
@@ -1,29 +1,34 @@
+import { Link } from 'react-router-dom';
+import Logo from '../assets/logo_main.png'
 function About() {
     return (
         <>
-        <header>
-        <div className="header-left">
-          <div className="logo"><img src={Logo} alt="" height={50} width={50} /></div>
-          <h1>GAMS</h1>
-        </div>
-        <div className="header-right">
-          <Link to="/"><button className="nav-link">Home</button></Link>
-          <Link to="/about"><button className="nav-link">About</button></Link>
-          <Link to="/contact"><button className="nav-link">Contact Us</button></Link>
-          <Link to="/login"><button className="nav-link">Login</button></Link>
-        </div>
-      </header>
+            <header>
+                <div className="header-left">
+                    <div className="logo"><img src={Logo} alt="" height={50} width={50} /></div>
+                    <h1>GAMS</h1>
+                </div>
+                <div className="header-right">
+                    <Link to="/"><button className="nav-link">Home</button></Link>
+                    <Link to="/about"><button className="nav-link">About</button></Link>
+                    <Link to="/contact"><button className="nav-link">Contact Us</button></Link>
+                    <Link to="/login"><button className="nav-link">Login</button></Link>
+                </div>
+            </header>
             <main>
                 <h1>About Us</h1>
-            <div className="top">
-                This mainly consists of
-                <ul>
-                    <li>License application for new course at collages.</li>
-                    <li>Upgrading license of an existing course at a certain collage.</li>
-                </ul>
-            </div>
+                <div className="top">
+                    This mainly consists of
+                    <ul>
+                        <li>License application for new course at collages.</li>
+                        <li>Upgrading license of an existing course at a certain collage.</li>
+                    </ul>
+                </div>
 
             </main>
+            <footer>
+                <p>&copy; 2025 Gradious Affiliation Management System. All rights reserved.</p>
+            </footer>
         </>
     );
 }
--- Frontend/src/components/Home.jsx
+++ Frontend/src/components/Home.jsx
@@ -1,4 +1,4 @@
-import { Link, Outlet } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import Logo from '../assets/logo_main.png';
 function Home() {
   return (
